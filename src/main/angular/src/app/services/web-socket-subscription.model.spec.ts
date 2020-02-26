6a065ed1530422f578c38efb1b71fd115972e771
kubikowkski <kubikowski0@gmail.com>
2020-02-26 16:39:29 -0500

    deleted spec files

--- src/main/angular/src/app/services/web-socket-subscription.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { WebSocketSubscription } from './web-socket-subscription.model';
-
-describe('WebSocketSubscription', () => {
-  it('should create an instance', () => {
-    expect(new WebSocketSubscription()).toBeTruthy();
-  });
-});
